ede984c1d8f8700f71c9cbddfe3f1f34f37cdfb5
Ryan Bohinski <rjbohinski@gmail.com>
2017-11-04 13:05:39 -0400

    Add argument error checking to the order endpoint.

--- source/RewardsService/rewardsservice/handlers/order_handler.py
+++ source/RewardsService/rewardsservice/handlers/order_handler.py
@@ -5,12 +5,15 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+import json
+import logging
+
 import bson.json_util
 import tornado.web
 from tornado.gen import coroutine
-import logging
-from db.customers import Customers
+from tornado.web import MissingArgumentError
 
+from db.customers import Customers
 
 
 class OrderHandler(tornado.web.RequestHandler):
@@ -24,12 +27,29 @@ class OrderHandler(tornado.web.RequestHandler):
         Requires the arguments 'email' and 'total'.
         """
         OrderHandler.logger.debug("OrderHandler.post()")
-        email = self.get_argument("email")
-        total = self.get_argument("total")
-        OrderHandler.logger.debug("Email: %s, Total: %s", email, total)
 
-        customer = Customers.auto_insert_update(email, total)
-        OrderHandler.logger.debug("Customer: %s", customer)
+        # Verify that the required arguments were provided.
+        error_data = []
+        try:
+            email = self.get_argument("email")
+        except MissingArgumentError:
+            error_data.append(
+                {"error": "MissingArgumentError",
+                 "detail": "The argument 'email' was not included in the message."})
+        try:
+            total = self.get_argument("total")
+        except MissingArgumentError:
+            error_data.append(
+                {"error": "MissingArgumentError",
+                 "detail": "The argument 'total' was not included in the message."})
 
-        self.write(bson.json_util.dumps(customer))
-        # self.write()
+        # If there were any errors, return the error message.
+        if error_data is not None:
+            self.write(json.dumps(error_data))
+        else:
+            OrderHandler.logger.debug("Email: %s, Total: %s", email, total)
+
+            customer = Customers.auto_insert_update(email, total)
+            OrderHandler.logger.debug("Customer: %s", customer)
+
+            self.write(bson.json_util.dumps(customer))
